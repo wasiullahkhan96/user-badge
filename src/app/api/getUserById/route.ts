5a55823f79865467e96b5185d492e4cd2a68fcc7
Wasiullah Khan <wasiullah.khan96@gmail.com>
2024-07-23 18:47:14 +0200

    fix import

--- src/app/api/getUserById/route.ts
+++ src/app/api/getUserById/route.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET(request: NextRequest) {
   const session = await getServerSession(authOptions);
